6ddebb263df5ce049c828d5488c70359c176e0a2
mohamedbsila <101298975+mohamedbsila@users.noreply.github.com>
2025-04-03 17:38:08 +0100

    Added logging to UniversiteServiceImpl and configured H2 database for testing

--- src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.kaddem.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class UniversiteServiceImpl implements IUniversiteService{
 @Autowired
     UniversiteRepository universiteRepository;
@@ -20,26 +22,32 @@ public class UniversiteServiceImpl implements IUniversiteService{
         // TODO Auto-generated constructor stub
     }
   public   List<Universite> retrieveAllUniversites(){
-return (List<Universite>) universiteRepository.findAll();
+    log.info("Fetching all universities...");
+    return (List<Universite>) universiteRepository.findAll();
     }
 
  public    Universite addUniversite (Universite  u){
-return  (universiteRepository.save(u));
+    log.info("Adding a new university: {}", u.getNomUniv());
+    return  (universiteRepository.save(u));
     }
 
  public    Universite updateUniversite (Universite  u){
+     log.info("Updating university with ID: {}", u.getIdUniv());
      return  (universiteRepository.save(u));
     }
 
   public Universite retrieveUniversite (Integer idUniversite){
-Universite u = universiteRepository.findById(idUniversite).get();
-return  u;
+    log.info("Retrieving university with ID: {}", idUniversite);
+    Universite u = universiteRepository.findById(idUniversite).get();
+    return  u;
     }
     public  void deleteUniversite(Integer idUniversite){
+        log.info("Deleting university with ID: {}", idUniversite);
         universiteRepository.delete(retrieveUniversite(idUniversite));
     }
 
     public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement){
+        log.info("Assigning university ID: {} to department ID: {}", idUniversite, idDepartement);
         Universite u= universiteRepository.findById(idUniversite).orElse(null);
         Departement d= departementRepository.findById(idDepartement).orElse(null);
         u.getDepartements().add(d);
@@ -47,7 +55,8 @@ return  u;
     }
 
     public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite){
-Universite u=universiteRepository.findById(idUniversite).orElse(null);
-return u.getDepartements();
+        log.info("Retrieving departments for university ID: {}", idUniversite);
+        Universite u=universiteRepository.findById(idUniversite).orElse(null);
+        return u.getDepartements();
     }
 }
